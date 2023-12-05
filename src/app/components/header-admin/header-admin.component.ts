f6bab8ef4681ad3bb1b6b524e51dcd060bbc1deb
Catalina Guaman <catalina16gv@gmail.com>
2023-12-05 18:47:14 -0500

    Agregamos funcionalidad Logout

--- src/app/components/header-admin/header-admin.component.ts
+++ src/app/components/header-admin/header-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-admin',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-admin.component.css']
 })
 export class HeaderAdminComponent {
-
+  constructor(private router: Router) {}
+  logout() {
+    localStorage.removeItem("token")
+    this.router.navigate(["login"])
+  }
 }
